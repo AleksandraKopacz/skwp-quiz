de867299159b32c19365a6e9feea2eb91b2b7f63
Aleksandra Kopacz <aleksandrakopacz4@gmail.com>
2026-05-15 00:35:55 +0200

    redirect

--- app/routes/quiz.tsx
+++ app/routes/quiz.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from "~/components/ui/NavBar";
 import type { Route } from "./+types/quiz";
 import { QuizForm } from "~/components/quiz/QuizForm";
+import { redirect } from "react-router";
 
 export function meta({ params }: Route.MetaArgs) {
   return [
@@ -9,7 +10,38 @@ export function meta({ params }: Route.MetaArgs) {
   ];
 }
 
-export async function loader({ params }: Route.LoaderArgs) {}
+export async function loader({ params }: Route.LoaderArgs) {
+  switch (params.chapterId) {
+    case "1":
+      break;
+    case "2":
+      break;
+    case "3":
+      break;
+    case "4":
+      break;
+    case "5":
+      break;
+    case "6":
+      break;
+    case "7":
+      break;
+    case "8":
+      break;
+    case "9":
+      break;
+    case "10":
+      break;
+    case "11":
+      break;
+    case "test":
+      break;
+    case "random":
+      break;
+    default:
+      throw redirect("/quiz/random");
+  }
+}
 
 export default function Quiz({ params }: Route.ComponentProps) {
   return (
